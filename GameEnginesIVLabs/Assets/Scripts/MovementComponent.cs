9bdfb57e5d48532fcc4bc08bd6e2b0ddd169907d
Julaxe <julaxe8@gmail.com>
2022-02-01 13:56:09 -0500

    Lab Week 3 done
    Gun imported, camera movement added -

--- GameEnginesIVLabs/Assets/Scripts/MovementComponent.cs
+++ GameEnginesIVLabs/Assets/Scripts/MovementComponent.cs
@@ -10,9 +10,16 @@ public class MovementComponent : MonoBehaviour
     public float walkSpeed = 5;
     public float runSpeed = 10;
     public float jumpForce = 5;
+
+    [Header("Camera")] 
+    public int maxAngle = 340;
+    public int minAngle = 40;
+    public float aimSensitivity = 1.0f;
+    public GameObject followObject;
     
-    private Vector2 inputDirection;
-    private Vector3 moveDirection;
+    private Vector2 _inputDirection;
+    private Vector3 _moveDirection;
+    private Vector2 _lookDirection;
 
     private PlayerController _playerController;
     private Animator _animator;
@@ -31,19 +38,40 @@ public class MovementComponent : MonoBehaviour
 
     private void Update()
     {
-        if (_playerController.isJumping) return;
-        if(inputDirection.magnitude == 0) moveDirection = Vector3.zero;
+        #region CameraMovement With Mouse
 
-        moveDirection = inputDirection.y * transform.forward + inputDirection.x * transform.right;
+        followObject.transform.rotation *= Quaternion.AngleAxis(_lookDirection.x * aimSensitivity * Time.deltaTime, Vector3.up);
+        followObject.transform.rotation *= Quaternion.AngleAxis(_lookDirection.y * aimSensitivity * Time.deltaTime, Vector3.left);
+
+        var angles = followObject.transform.eulerAngles;
+        angles.z = 0;
+        var angle = followObject.transform.localEulerAngles.x;
+        
+        if (angle > 180 && angle < maxAngle)
+        {
+            angles.x = maxAngle;
+        }else if (angle < 180 && angle > minAngle)
+        {
+            angles.x = minAngle;
+        }
+
+        followObject.transform.localEulerAngles = angles;
+        #endregion
+        
+        if (_playerController.isJumping) return;
+        if(_inputDirection.magnitude == 0) _moveDirection = Vector3.zero;
+
+        _moveDirection = _inputDirection.y * transform.forward + _inputDirection.x * transform.right;
         float currentSpeed = _playerController.isRunning ? runSpeed : walkSpeed;
-        transform.position += moveDirection * (currentSpeed * Time.deltaTime);
+        transform.position += _moveDirection * (currentSpeed * Time.deltaTime);
+        
     }
 
     public void OnMovement(InputValue value)
     {
-        inputDirection = value.Get<Vector2>();
-        _animator.SetFloat(movementXHash,  inputDirection.x);
-        _animator.SetFloat(movementYHash, inputDirection.y);
+        _inputDirection = value.Get<Vector2>();
+        _animator.SetFloat(movementXHash,  _inputDirection.x);
+        _animator.SetFloat(movementYHash, _inputDirection.y);
     }
 
     public void OnJump(InputValue value)
@@ -52,6 +80,10 @@ public class MovementComponent : MonoBehaviour
         _animator.SetBool(jumpHash, _playerController.isJumping);
     }
 
+    public void OnLook(InputValue value)
+    {
+        _lookDirection = value.Get<Vector2>();
+    }
     public void OnRun(InputValue value)
     {
         _playerController.isRunning = value.isPressed;
